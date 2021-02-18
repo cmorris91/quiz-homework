991e3b97942246a57c72bc84935da213b70abc56
Chelsey Morris <chelseymorris91@yahoo.com>
2021-02-17 20:39:17 -0800

    added questions, started askQuestion function

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,44 @@
 var timeEl = document.querySelector(".timer");
 var highscoreButton = document.querySelector("#highscores");
-var startButton = document.querySelector("start-button");
+var startButton = document.querySelector("#start-button");
+var header = document.querySelector("h1");
+var mainEl = document.querySelector("main");
+
+var question1 = {
+    question: "What is a JavaScript element that represents either TRUE or FALSE values?",
+    choices: ["Event", "Boolean", "tag", "footer"],
+    answer: "Boolean"
+}
+        
+var question2 = {
+    question: "What element is used in Javascript to store data?",
+    choices: ["Syntax", "Node", "HTML", "Variable"],
+    answer: "Variable"
+
+}
+var question3 = {
+    question: "What is Javascript?",
+    choices: ["Person", "Book", "Programming Language", "Storage"],
+    answer: "Programming Language"
+}
+var question4 = {
+    question: "Which element let's you store multiple values in 1 variable?",
+    choices: ["Array", "Event", "Function", "Stylesheet"],
+    answer: "Array"
+
+}
+        console.log(question4.question);
 
 var secondsLeft = 60;
 
-
 startButton.addEventListener("click", function() {
+    var header = document.querySelector("h1");
+    header.remove();
+    startButton.remove();
     timeLeft();
-    // call function that will ask question goes her
-})
+    askQuestions();
+
+});
 
 
 
@@ -25,5 +55,36 @@ function timeLeft() {
 
     }, 1000);
     return;
-}
+};
 
+function askQuestions() {
+    var questionBox = document.createElement("h2");
+    questionBox.textContent(questions.question);
+    mainEl.appendChild(questionBox);
+
+    var index = 0;
+    var quesitonsObj = questions[index]
+    for(var i = 0; i < quesitonsObj.choices.length; i++) {
+        var choicesElement = document.createElement("button")
+        choicesElement.setAttribute("class", "choices")
+        choicesElement.setAttribute("data-value", quesitonsObj.choices[i])
+        choicesElement.textContent = quesitonsObj.choices[i]
+        document.body.appendChild(choicesElement);
+    };
+    document.body.addEventListener("click", function(event) {
+        // console.log(event)
+        if(event.target.className === "choices") {
+            var userChoice = event.target.textContent
+            if(userChoice === quesitonsObj.answer) {
+                console.log("you got it correct")
+                console.log("iterate next question")
+                index++
+                console.log(index)
+                console.log(questions[index])
+            }
+        }
+    
+    
+ 
+});
+}
